d0f9ef6ce2eb6de8fbeaf2c05c1cb92a112161bd
Кучуков Александр Сафронович <Kuchukov.A.Safr@sberbank.ru>
2023-04-16 02:00:53 +0300

    consume

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"sort"
-	"sync"
 	"time"
 
 	"github.com/twmb/franz-go/pkg/kgo"
@@ -32,7 +31,7 @@ func run() {
 	ctx := context.Background()
 
 	var records = map[int]string{}
-	for i := 0; i <= 2; i++ {
+	for i := 0; i <= 1000; i++ {
 		records[i] = fmt.Sprintf("Hello sky - %d", i)
 	}
 	sortedKeys := make([]int, 0, len(records))
@@ -42,22 +41,22 @@ func run() {
 	}
 	sort.Ints(sortedKeys)
 
-	var wg sync.WaitGroup
-	t1 := time.Now()
-	for idx := range sortedKeys {
-		key := fmt.Sprintf("async-index-%d", idx)
-		wg.Add(1)
-		record := &kgo.Record{Topic: "beat-output", Key: []byte(key), Value: []byte(records[idx])}
-		cl.Produce(ctx, record, func(_ *kgo.Record, err error) {
-			defer wg.Done()
-			if err != nil {
-				fmt.Printf("record had a produce error: %v\n", err)
-			}
-		})
-		wg.Wait()
-	}
-	t2 := time.Now()
-	fmt.Println(t2.Sub(t1))
+	// var wg sync.WaitGroup
+	// t1 := time.Now()
+	// for idx := range sortedKeys {
+	// 	key := fmt.Sprintf("async-index-%d", idx)
+	// 	wg.Add(1)
+	// 	record := &kgo.Record{Topic: "beat-output", Key: []byte(key), Value: []byte(records[idx])}
+	// 	cl.Produce(ctx, record, func(_ *kgo.Record, err error) {
+	// 		defer wg.Done()
+	// 		if err != nil {
+	// 			fmt.Printf("record had a produce error: %v\n", err)
+	// 		}
+	// 	})
+	// 	wg.Wait()
+	// }
+	// t2 := time.Now()
+	// fmt.Println(t2.Sub(t1))
 
 	// 1.) Producing a message
 	// All record production goes through Produce, and the callback can be used
@@ -66,46 +65,49 @@ func run() {
 	// // Alternatively, ProduceSync exists to synchronously produce a batch of records.
 	fmt.Printf("\n\n\n")
 	t3 := time.Now()
-	var recordmap = []kgo.Record{}
+	var recordmap = []*kgo.Record{}
 	for idx := range sortedKeys {
 		key := fmt.Sprintf("index-%d", idx)
 		record := &kgo.Record{Topic: "beat-output", Key: []byte(key), Value: []byte(records[idx])}
-		recordmap = append(recordmap, *record)
+		recordmap = append(recordmap, record)
 	}
 
-	if err := cl.ProduceSync(ctx, &recordmap[0], &recordmap[1], &recordmap[2]).FirstErr(); err != nil {
+	if err := cl.ProduceSync(ctx, recordmap...).FirstErr(); err != nil {
 		fmt.Printf("record had a produce error while synchronously producing: %v\n", err)
 	}
 
 	t4 := time.Now()
 	fmt.Println(t4.Sub(t3))
 
-	// // 2.) Consuming messages from a topic
-	// for {
-	// 	fetches := cl.PollFetches(ctx)
-	// 	if errs := fetches.Errors(); len(errs) > 0 {
-	// 		// All errors are retried internally when fetching, but non-retriable errors are
-	// 		// returned from polls so that users can notice and take action.
-	// 		panic(fmt.Sprint(errs))
-	// 	}
+	fmt.Println("Consume messages...")
+	// 2.) Consuming messages from a topic
+	for {
+		t10 := time.Now()
+		fetches := cl.PollFetches(ctx)
+		if errs := fetches.Errors(); len(errs) > 0 {
+			// All errors are retried internally when fetching, but non-retriable errors are
+			// returned from polls so that users can notice and take action.
+			panic(fmt.Sprint(errs))
+		}
 
-	// 	// We can iterate through a record iterator...
-	// 	iter := fetches.RecordIter()
-	// 	for !iter.Done() {
-	// 		record := iter.Next()
-	// 		fmt.Println(string(record.Value), "from an iterator!")
-	// 	}
+		// We can iterate through a record iterator...
+		iter := fetches.RecordIter()
+		for !iter.Done() {
+			record := iter.Next()
+			fmt.Println(string(record.Value), "from an iterator!")
+		}
+		t11 := time.Now()
+		fmt.Println(t11.Sub(t10))
+		// or a callback function.
+		// fetches.EachPartition(func(p kgo.FetchTopicPartition) {
+		// 	for _, record := range p.Records {
+		// 		fmt.Println(string(record.Value), "from range inside a callback!")
+		// 	}
 
-	// 	// or a callback function.
-	// 	fetches.EachPartition(func(p kgo.FetchTopicPartition) {
-	// 		for _, record := range p.Records {
-	// 			fmt.Println(string(record.Value), "from range inside a callback!")
-	// 		}
-
-	// 		// We can even use a second callback!
-	// 		p.EachRecord(func(record *kgo.Record) {
-	// 			fmt.Println(string(record.Value), "from a second callback!")
-	// 		})
-	// 	})
-	// }
+		// 	// We can even use a second callback!
+		// 	p.EachRecord(func(record *kgo.Record) {
+		// 		fmt.Println(string(record.Value), "from a second callback!")
+		// 	})
+		// })
+	}
 }
